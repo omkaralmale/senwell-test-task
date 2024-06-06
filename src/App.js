55a4557c4b16c4ffc8447aea1c8784c037bcf4b4
Omkar Almale <omkaralmale@gmail.com>
2024-06-06 13:55:30 +0000

    working omn hover funtionality

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Table from './Table';
+
+function App() {
+  const [data, setData] = useState();
+
+  const callApi=()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log('Error fetching users:', error));
+  }
+
+  useEffect(() => {
+    callApi()
+  }, [])
+  console.log(data);
+
+  const handleDelete=(id)=>{
+    setData(data.filter((item)=>item?.id !==id ))
+  }
+
+  const onReset=()=>{
+ callApi()
+  }
+
+
+  return (
+    <div className="App">
+      <Table list={data} handleDelete={(id)=>handleDelete(id)} onReset={onReset}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+let timeoutId;
+const Table = ({ list,handleDelete,onReset }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(list || []);
+
+const handleSearchChange = (value) => {
+  setSearchTerm(value);
+
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    const filtered = (list || []).filter(user =>
+      user.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  }, 1000);
+};
+
+
+  useEffect(()=>{
+    if (list) {
+        setFilteredUsers(list)
+    }
+  },[list])
+
+  const handleDeleteId=(id)=>{
+    handleDelete(id)
+  }
+  const handleReset=()=>{
+onReset()
+  }
+
+  const header=["ID","NAME","COMPANY","ADDRESS","ACTION"]
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by Name"
+        value={searchTerm}
+        onChange={(e) => handleSearchChange(e.target.value)}
+        className="border border-gray-300 rounded-full w-60 px-3 py-2 m-2 placeholder:text-sm"
+      />
+      <button onClick={handleReset} className="bg-black hover:bg-gray-600 text-white px-4  py-2 rounded-full ">RESET</button>
+      <table className="w-3/4 border-collapse rounded border border-gray-300 m-2">
+        <thead>
+          <tr>
+            {header.map((item,index)=><th key={index} className="border border-gray-300 p-2 text-sm">{item}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {filteredUsers.map((user, index) => (
+            <tr key={user.id}>
+              <td className="border border-gray-300 px-2 text-center text-sm">{user.id}</td>
+              <td className="border border-gray-300 px-2 text-sm">{user.name}</td>
+              <td className="border border-gray-300 px-2 text-sm">
+                <div
+                  className="relative"
+                >
+                  <div className="text-sm">{user.company.name}</div>
+                  <div className="text-sm absolute bg-white border border-gray-300 px-2 rounded shadow-md hidden opacity-0 transition duration-300 ease-in-out transform translate-y-2 hover:opacity-100 hover:block" id={`company-details-${index}`}>
+                    <p className="text-sm">{`Catchphrase: ${user.company.catchPhrase}`}</p>
+                    <p className="text-sm">{`BS: ${user.company.bs}`}</p>
+                  </div>
+                </div>
+              </td>
+              <td className="text-sm border border-gray-300 px-2">{user.address.street}, {user.address.city}, {user.address.zipcode}</td>
+              <td className="text-sm border border-gray-300 px-2">
+                <button onClick={() => handleDeleteId(user.id)} className="bg-red-700 w-full hover:bg-red-900 text-white my-1 px-2 py-1 rounded-full text-sm">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
